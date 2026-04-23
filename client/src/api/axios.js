c56491b8784acf264068b00771ee8151f0d03d76
= <=>
2026-04-23 20:37:38 +0530

    Use VITE_API_URL env var in axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 const api = axios.create({
-  baseURL: 'https://ampereone.onrender.com/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'https://ampereone.onrender.com/api/v1',
   headers: { 'Content-Type': 'application/json' },
 });
 
